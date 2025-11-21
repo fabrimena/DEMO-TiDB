a7e467951324e171a13be2bc0c77fd9d788a7a96
Fabricio Mena Mejia <48070782+fabrimena@users.noreply.github.com>
2025-11-20 18:01:11 -0600

    Update insert_accounts.py

--- insert_accounts.py
+++ insert_accounts.py
@@ -1,23 +1,15 @@
 import mysql.connector
 
 POOL_CONFIG = {
-    'host': 'tu-HOST',
+    'host': 'gateway01.us-east-1.prod.aws.tidbcloud.com',
     'port': 4000,
-    'user': 'tu-USERNAME',
-    'password': 'tu-PASSWORD',
+    'user': '4SUir1NzoHpaiZR.root',
+    'password': 'drE83TL7cJpCSrRE',
     'database': 'bank'
 }
 
 conn = mysql.connector.connect(**POOL_CONFIG)
 cur = conn.cursor()
-
-# Insertar las 3 cuentas originales
-cur.execute("""
-    INSERT INTO accounts (name, balance) VALUES
-    ('Alice', 1000.00),
-    ('Bob', 500.00),
-    ('Carol', 2000.00)
-""")
 
 # Insertar 97 cuentas adicionales
 additional_accounts = [(f'User_{i}', 1000.00 + i*10) for i in range(1, 98)]
